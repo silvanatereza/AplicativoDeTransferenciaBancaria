d041d33051825f6a613355f4a79a446c230a097e
s.da.silva.rocha <s.da.silva.rocha@WINAPUZRJRTUWWD>
2023-01-21 00:59:58 -0300

    Adicionar arquivos de projeto.

---
+++ TransferenciasBancarias/Classes/Conta.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransferenciasBancarias
+{
+    internal class Conta
+    {
+        private TipoConta TipoConta { get; set; }
+
+        private double Saldo { get; set; }
+
+        private double Credito { get; set; }
+
+        private string Nome { get; set; }
+
+
+        public Conta(TipoConta tipoConta, double saldo, double credito, string nome)
+        {
+            this.TipoConta = tipoConta;
+            this.Saldo = saldo;
+            this.Credito = credito;
+            this.Nome = nome;
+        }
+
+        public bool Sacar (double valorSaque)
+        {
+            if (this.Saldo - valorSaque < (this.Credito * .1))
+            {
+                Console.WriteLine("Saldo insuficiente!");
+                return false;
+            }
+
+            this.Saldo -= valorSaque;
+
+            Console.WriteLine("Saldo atual da conta de {0} é {1}", this.Nome, this.Saldo);
+
+            return true;
+        }
+
+        public void Depositar (double valorDeposito)
+
+        {
+            this.Saldo+= valorDeposito;
+           
+            Console.WriteLine("Saldo atual da conta de {0} é {1}", this.Nome, this.Saldo);
+        }
+
+        public void Trensferir(double valorTranferencia, Conta contaDestino)
+        {
+            if (this.Sacar(valorTranferencia))
+            {
+                contaDestino.Depositar(valorTranferencia);
+            }
+      
+     
+        }
+
+        public override string ToString()
+        {
+            string retorno = "";
+            retorno += "TipoConta " + this.TipoConta + " | ";
+            retorno += "Nome " + this.Nome + " | ";
+            retorno += "Saldo " + this.Saldo + " | ";
+            retorno += "Crédito " + this.Credito;
+            return retorno;
+
+        }
+
+        internal void Transferir(double valorTransferencia, Conta conta)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
---
+++ TransferenciasBancarias/Enum/TopoConta.cs
@@ -0,0 +1,14 @@
+﻿
+
+
+
+namespace TransferenciasBancarias
+{
+    public enum TipoConta
+    {
+        PessoaFisica = 1,
+       
+        PessoaJuridica = 2, 
+    }
+
+}
